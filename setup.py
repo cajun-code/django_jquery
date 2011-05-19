b54e815359063cf6f7a015e4cfc1eb59ce85b7a6
Allan Davis <cajun.code@gmail.com>
2011-05-19 07:08:46 -0400

    modified setup.py and added keep for static folder

---
+++ setup.py
@@ -0,0 +1,27 @@
+#! /usr/bin/env python
+
+from distutils.core import setup
+
+
+requirements = [
+    "Django >= 1.3",
+]
+
+
+setup(
+    name = "django_jquery",   
+    version              = version,
+    description          = "Django library to static inlcude jquery",
+    long_description     = None,
+    classifiers          = [],
+    keywords             = 'django, jquery',
+    author               = 'Allan Davis',
+    author_email         = 'cajun.code@gmail.com',
+    url                  = 'http://github.com/cajun-code/django_jquery',
+    license              = 'BSD',
+    packages             = find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    include_package_data = True,
+    scripts              = ['bin/jquery_update'],
+    zip_safe             = False,
+    install_requires     = requirements,
+)
