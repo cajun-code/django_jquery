663480b6bad11e1bab1dc871b38a60c0e5a69859
Allan Davis <cajun.code@gmail.com>
2011-05-20 07:30:51 -0400

    updated jquery tags

--- jquery/templatetags/jquery_tags.py
+++ jquery/templatetags/jquery_tags.py
@@ -7,7 +7,7 @@ register = template.Library()
 
 @register.simple_tag
 def jquery():
-    str = "<script type='text/javascript' src='{0}jquery-min.js' ></script> "
+    str = "<script type='text/javascript' src='{0}jquery.min.js' ></script> "
 
 @register.simple_tag
 def jquery_ui():
